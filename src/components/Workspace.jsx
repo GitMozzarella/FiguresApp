4e532049a951a94001a350a7397bf29e5f90f7fb
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-01-17 20:59:44 +0600

    fix(workspace) : перенес функции внутрь useEffectов

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef , useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Rectangle from './figures/Rectangle';
 import Triangle from './figures/Triangle';
 
@@ -10,42 +10,30 @@ const Workspace = ({
 }) => {
   const workspaceRef = useRef(null);
 
-  //1. Поехало форматирование во всём файле (отступы, пустые строки, console.log-и)
-  //2. Эту функциия не используется за пределами useEffect, лучше её поместить внутрь. Аналогично с эффектом ниже.
-  // Если бы было больше логики, то такие эффекти получилось бы без проблем вынести в отдельный хук, аля useWorkspaceEffects() для удобства чтения
-  const figuresOutsideClick = (e) => {
-	const isClickOutside = e.target.classList.contains('workspace');
-
-	isClickOutside ? setSelectedFigures(null) : null;
- };
-
   useEffect(() => {
+    const figuresOutsideClick = (e) => {
+      const isClickOutside = e.target.classList.contains('workspace');
+      isClickOutside ? setSelectedFigures(null) : null;
+    };
     const workspace = workspaceRef.current;
     workspace.addEventListener('click', figuresOutsideClick);
-
     return () => {
       workspace.removeEventListener('click', figuresOutsideClick);
     };
   }, [setSelectedFigures]);
 
-const handleDelete = (e) => {
-  e.key === 'Delete' && selectedFigures !== null ? (
-    setSelectedFigures(null),
-    deleteFigure()
-  ) : null;
-};
-
-
   useEffect(() => {
+    const handleDelete = (e) => {
+      e.key === 'Delete' && selectedFigures !== null
+        ? (setSelectedFigures(null), deleteFigure())
+        : null;
+    };
+
     window.addEventListener('keydown', handleDelete);
     return () => {
       window.removeEventListener('keydown', handleDelete);
     };
   }, [selectedFigures, figures]);
-
-console.log(workspaceRef);
-
-
 
   return (
     <div className="workspace" ref={workspaceRef}>
@@ -58,8 +46,7 @@ console.log(workspaceRef);
             setSelectedFigures={setSelectedFigures}
             key={figure.id}
             deleteFigure={deleteFigure}
-				workspaceRef={workspaceRef}
-			
+            workspaceRef={workspaceRef}
           />
         ) : (
           <Triangle
@@ -69,7 +56,7 @@ console.log(workspaceRef);
             setSelectedFigures={setSelectedFigures}
             key={figure.id}
             deleteFigure={deleteFigure}
-				workspaceRef={workspaceRef}
+            workspaceRef={workspaceRef}
           />
         ),
       )}
